a91a88bb1a6fb1a9ef8762ee242a7d07f5b2e753
B4Le <iqbalanggoro48@gmail.com>
2021-01-15 18:10:51 +0700

    Python Study II

---
+++ Python Study II/page7/script.py
@@ -0,0 +1,5 @@
+fruits = {'apple': 'manzana', 'orange': 'naranja', 'grape': 'uva'}
+
+# With a for loop, print '___ is ___ in Spanish'
+for fruit_key in fruits:
+    print(fruit_key + ' is' + fruits[fruit_key] + ' in Spanish')
